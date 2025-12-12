8fb480b58d02ba5336d5ad837e52152ff99f369d
Riteshraj Shetage <144100040+riteshraj-shetage@users.noreply.github.com>
2025-12-12 13:54:53 +0530

    Add script to manage iframe interactions
    Implement iframe click event to pause other iframes.

---
+++ spotify-collabs/scripts/one_play.js
@@ -0,0 +1,15 @@
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const iframes = document.querySelectorAll("iframe");
+
+    iframes.forEach((iframe) => {
+      iframe.addEventListener("click", () => {
+        iframes.forEach((other) => {
+          if (other !== iframe) {
+            other.src = other.src;
+          }
+        });
+      });
+    });
+  });
+</script>
